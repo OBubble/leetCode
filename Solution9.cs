73b4f5904a161e6220665d13d0bc839afb8d65e7
OBubble <3337250176@qq.com>
2020-01-13 23:27:21 +0800

    1.13.2

---
+++ Solution9.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false;
+        int[] res = new int[10];
+        int index = 0;
+        while (x != 0)
+        {
+            res[index++] = x % 10;
+            x /= 10;
+        }
+        for (int i = 0; i < index/2; i++)
+        {
+            if (res[index - i - 1] != res[i])
+                return false;
+        }
+        return true;
+    }
+}
